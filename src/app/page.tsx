33acb270ce924668db42e8d48a4918a96308b7c3
MertenD <merten.dieckmann@web.de>
2024-03-12 22:37:08 +0100

    Add shadcn/ui library and first test with resizable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import BpmnEditor from "@/components/processEditor/modules/flow/BpmnEditor";
-import Image from "next/image";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 export default function Home() {
   return (
     <div style={{ height: "100vh" }}>
-      <BpmnEditor />
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel>
+          Processes
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          <div style={{ height: "100%", width: "100%", paddingLeft: 5 }}>
+            <BpmnEditor />
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
