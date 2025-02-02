198521bea7812b0f5e9d4d84cad1414a6e682b8a
MertenD <merten.dieckmann@web.de>
2025-02-02 15:21:28 +0100

    Move middleware in correct directory and add current path header

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request)
+  response.headers.set("x-current-path", request.nextUrl.pathname);
+  return response;
 }
 
 export const config = {
