0ac80e8baa1f15a176fbae7fdd11f745242b9cab
MertenD <merten.dieckmann@web.de>
2025-02-02 22:05:43 +0100

    Add gamification options only to manual activities and

--- src/actions/shop/get-node-definition.ts
+++ src/actions/shop/get-node-definition.ts
@@ -52,9 +52,8 @@ export default async function getNodeDefinition(nodeDefinitionId: number): Promi
         ...nodeDefinition.optionsDefinition.structure
     ]
 
-    // Add role selection for manual nodes
     if (nodeDefinition.executionMode === "Manual") {
-        console.log("Adding role selection")
+        // Add role selection for manual nodes
         nodeDefinition.optionsDefinition.structure.push(
             {
                 type: OptionsStructureType.SEPARATOR
@@ -67,107 +66,106 @@ export default async function getNodeDefinition(nodeDefinitionId: number): Promi
                 options: [ { values: [ OptionsStructureSpecialValues.AVAILABLE_ROLES ] } ]
             } as OptionsSelect
         )
-        console.log(nodeDefinition)
-    }
 
-    // Add gamification options
-    nodeDefinition.optionsDefinition.structure.push(
-        {
-            type: OptionsStructureType.SEPARATOR
-        } as OptionsSeparator,
-        {
-            type: OptionsStructureType.SELECT,
-            label: "Gamification type",
-            defaultValue: GamificationType.NONE,
-            keyString: "gamificationType",
-            options: [
-                {
-                    values: [GamificationType.NONE],
-                    dependentStructure: []
-                },
-                {
-                    values: [GamificationType.POINTS],
-                    dependentStructure: [
-                        {
-                            type: OptionsStructureType.SELECT,
-                            label: "Points type",
-                            defaultValue: PointsType.EXPERIENCE,
-                            keyString: "gamificationOptions.pointType",
-                            options: [
-                                { values: Object.values(PointsType) }
-                            ]
-                        } as OptionsSelect,
-                        {
-                            type: OptionsStructureType.ROW,
-                            structure: [
-                                {
-                                    type: OptionsStructureType.SELECT,
-                                    label: "Effect",
-                                    defaultValue: PointsApplicationMethod.INCREMENT_BY,
-                                    keyString: "gamificationOptions.pointsApplicationMethod",
-                                    options: [
-                                        { values: Object.values(PointsApplicationMethod) }
-                                    ]
-                                } as OptionsSelect,
-                                {
-                                    type: OptionsStructureType.INPUT,
-                                    label: "Amount",
-                                    placeholder: "20",
-                                    keyString: "gamificationOptions.pointsForSuccess"
-                                } as OptionsInput
-                            ]
-                        } as OptionsRow,
-                        {
-                            type: OptionsStructureType.SEPARATOR
-                        } as OptionsSeparator,
-                        {
-                            type: OptionsStructureType.CHECKBOX,
-                            defaultValue: false,
-                            label: "Gamification condition",
-                            keyString: "gamificationOptions.hasCondition",
-                            options: [
-                                {
-                                    values: [true],
-                                    dependentStructure: [
-                                        {
-                                            type: OptionsStructureType.SELECT_WITH_CUSTOM,
-                                            label: "Value 1",
-                                            keyString: "gamificationOptions.value1",
-                                            options: [ { values: [ OptionsStructureSpecialValues.AVAILABLE_VARIABLES ] } ]
-                                        } as OptionsSelectWithCustom,
-                                        {
-                                            type: OptionsStructureType.SELECT,
-                                            label: "Comparison",
-                                            keyString: "gamificationOptions.comparison",
-                                            defaultValue: Comparisons.EQUALS,
-                                            options: [ { values: Object.values(Comparisons) } ]
-                                        } as OptionsSelect,
-                                        {
-                                            type: OptionsStructureType.SELECT_WITH_CUSTOM,
-                                            label: "Value 2",
-                                            keyString: "gamificationOptions.value2",
-                                            options: [ { values: [ OptionsStructureSpecialValues.AVAILABLE_VARIABLES ] } ]
-                                        } as OptionsSelectWithCustom
-                                    ]
-                                }
-                            ]
-                        } as OptionsCheckbox,
-                    ]
-                },
-                {
-                    values: [GamificationType.BADGES],
-                    dependentStructure: [
-                        {
-                            type: OptionsStructureType.INPUT,
-                            label: "Badge type",
-                            keyString: "gamificationOptions.badgeType",
-                            suggestions: Object.values(BadgeType),
-                        } as OptionsInput
-                    ]
-                }
-            ]
-        } as OptionsSelect
-    )
+        // Add gamification options for manual nodes
+        nodeDefinition.optionsDefinition.structure.push(
+            {
+                type: OptionsStructureType.SEPARATOR
+            } as OptionsSeparator,
+            {
+                type: OptionsStructureType.SELECT,
+                label: "Gamification type",
+                defaultValue: GamificationType.NONE,
+                keyString: "gamificationType",
+                options: [
+                    {
+                        values: [GamificationType.NONE],
+                        dependentStructure: []
+                    },
+                    {
+                        values: [GamificationType.POINTS],
+                        dependentStructure: [
+                            {
+                                type: OptionsStructureType.SELECT,
+                                label: "Points type",
+                                defaultValue: PointsType.EXPERIENCE,
+                                keyString: "gamificationOptions.pointType",
+                                options: [
+                                    { values: Object.values(PointsType) }
+                                ]
+                            } as OptionsSelect,
+                            {
+                                type: OptionsStructureType.ROW,
+                                structure: [
+                                    {
+                                        type: OptionsStructureType.SELECT,
+                                        label: "Effect",
+                                        defaultValue: PointsApplicationMethod.INCREMENT_BY,
+                                        keyString: "gamificationOptions.pointsApplicationMethod",
+                                        options: [
+                                            { values: Object.values(PointsApplicationMethod) }
+                                        ]
+                                    } as OptionsSelect,
+                                    {
+                                        type: OptionsStructureType.INPUT,
+                                        label: "Amount",
+                                        placeholder: "20",
+                                        keyString: "gamificationOptions.pointsForSuccess"
+                                    } as OptionsInput
+                                ]
+                            } as OptionsRow,
+                            {
+                                type: OptionsStructureType.SEPARATOR
+                            } as OptionsSeparator,
+                            {
+                                type: OptionsStructureType.CHECKBOX,
+                                defaultValue: false,
+                                label: "Gamification condition",
+                                keyString: "gamificationOptions.hasCondition",
+                                options: [
+                                    {
+                                        values: [true],
+                                        dependentStructure: [
+                                            {
+                                                type: OptionsStructureType.SELECT_WITH_CUSTOM,
+                                                label: "Value 1",
+                                                keyString: "gamificationOptions.value1",
+                                                options: [ { values: [ OptionsStructureSpecialValues.AVAILABLE_VARIABLES ] } ]
+                                            } as OptionsSelectWithCustom,
+                                            {
+                                                type: OptionsStructureType.SELECT,
+                                                label: "Comparison",
+                                                keyString: "gamificationOptions.comparison",
+                                                defaultValue: Comparisons.EQUALS,
+                                                options: [ { values: Object.values(Comparisons) } ]
+                                            } as OptionsSelect,
+                                            {
+                                                type: OptionsStructureType.SELECT_WITH_CUSTOM,
+                                                label: "Value 2",
+                                                keyString: "gamificationOptions.value2",
+                                                options: [ { values: [ OptionsStructureSpecialValues.AVAILABLE_VARIABLES ] } ]
+                                            } as OptionsSelectWithCustom
+                                        ]
+                                    }
+                                ]
+                            } as OptionsCheckbox,
+                        ]
+                    },
+                    {
+                        values: [GamificationType.BADGES],
+                        dependentStructure: [
+                            {
+                                type: OptionsStructureType.INPUT,
+                                label: "Badge type",
+                                keyString: "gamificationOptions.badgeType",
+                                suggestions: Object.values(BadgeType),
+                            } as OptionsInput
+                        ]
+                    }
+                ]
+            } as OptionsSelect
+        )
+    }
 
     return nodeDefinition
 }
