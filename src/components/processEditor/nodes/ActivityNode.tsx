f2678574195f263b21c98026ee62b3f52d1ce74b
MertenD <merten.dieckmann@web.de>
2025-02-03 19:21:32 +0100

    Add icon to activity on canvas

--- src/components/processEditor/nodes/ActivityNode.tsx
+++ src/components/processEditor/nodes/ActivityNode.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+"use client"
+
+import React, {useEffect} from 'react';
 import {Handle, NodeProps, Position} from 'reactflow';
-import {handleStyle, selectedColor} from "@/stores/store";
+import {handleStyle} from "@/stores/store";
 import {GamificationType} from "@/model/GamificationType";
 import {ActivityType} from "@/model/ActivityType";
 import {PointsType} from "@/model/PointsType";
 import {BadgeType} from "@/model/BadgeType";
 import {Comparisons} from "@/model/Comparisons";
+import {NodeDefinition} from "@/model/NodeDefinition";
+import getNodeDefinition from "@/actions/shop/get-node-definition";
+import DynamicIcon from "@/components/DynamicIcon";
 
 export type ActivityNodeData = {
     backgroundColor?: string,
@@ -35,17 +40,26 @@ export type ActivityNodeData = {
 
 export default function ActivityNode({ id, selected, data }: NodeProps<ActivityNodeData>) {
 
+    const [nodeDefinition, setNodeDefinition] = React.useState<NodeDefinition | null>(null)
+
+    useEffect(() => {
+        if (data.nodeDefinitionId) {
+            getNodeDefinition(data.nodeDefinitionId).then((nodeDefinition) => {
+                setNodeDefinition(nodeDefinition)
+            })
+        }
+    }, [data]);
+
     return (
         <div style={{
             ...activityShapeStyle,
             backgroundColor: data.backgroundColor,
             boxShadow: selected ? `0px 0px 5px 1px #14803c` : undefined
-        }} className={`bg-background border-2 border-foreground ${ selected ? "border-primary" : "" }`} >
+        }} className={`flex flex-row gap-x-2 p-2 items-center bg-background border-2 border-foreground ${ selected ? "border-primary" : "" }`} >
             <Handle style={handleStyle} type="source" position={Position.Right}/>
             <Handle style={handleStyle} type="target" position={Position.Left}/>
-            <div className="flex flex-col p-2 items-center">
-                <div>{ data.task || "-" }</div>
-            </div>
+            <DynamicIcon name={nodeDefinition?.icon} className={`${selected ? "text-primary" : ""}`} />
+            <p className={`${selected ? "text-primary" : ""}`}>{ data.task || "-" }</p>
         </div>
     )
 }
