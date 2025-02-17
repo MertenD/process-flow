af7b0f5025fce5dfa58d48a310ac852fb2013056
MertenD <merten.dieckmann@web.de>
2025-02-17 16:27:19 +0100

    Add activities from node definitions to on canvas node selector

--- src/components/processEditor/toolbars/OnCanvasNodesSelector.tsx
+++ src/components/processEditor/toolbars/OnCanvasNodesSelector.tsx
@@ -9,17 +9,24 @@ import { endNodeShapeStyle } from "../nodes/EndNode"
 import { activityShapeStyle } from "../nodes/ActivityNode"
 import { GatewayShapeStyle } from "../nodes/GatewayNode"
 import { NodeTypes } from "@/model/NodeTypes"
+import {Input} from "@/components/ui/input";
+import DynamicIcon from "@/components/DynamicIcon";
+import type {NodeDefinitionPreview} from "@/model/NodeDefinition";
+import {Flag, Plus} from "lucide-react";
 
 export interface OnCanvasNodesToolbarProps {
     open: boolean
     position: { x: number; y: number }
-    onClose: (nodeType: NodeTypes | null) => void
+    onClose: (nodeType: NodeTypes | null, nodeData?: any) => void
+    nodeDefinitionPreviews: NodeDefinitionPreview[]
 }
 
 export default function OnCanvasNodesToolbar(props: OnCanvasNodesToolbarProps) {
     const { onClose, open, position } = props
     const width = 240
     const height = 350
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const [filteredActivities, setFilteredActivities] = useState<NodeDefinitionPreview[]>(props.nodeDefinitionPreviews)
 
     const [windowWidth, setWindowWidth] = useState(0)
     const [windowHeight, setWindowHeight] = useState(0)
@@ -36,26 +43,36 @@ export default function OnCanvasNodesToolbar(props: OnCanvasNodesToolbarProps) {
         return () => window.removeEventListener('resize', updateWindowDimensions)
     }, [])
 
+    useEffect(() => {
+        setFilteredActivities(
+            props.nodeDefinitionPreviews.filter((activity) => activity.name.toLowerCase().includes(searchTerm.toLowerCase())),
+        )
+    }, [searchTerm, props.nodeDefinitionPreviews])
+
     const handleClose = () => {
         onClose(null)
     }
 
-    const handleNodeSelected = (nodeType: NodeTypes) => {
-        onClose(nodeType)
+    const handleNodeSelected = (nodeType: NodeTypes, nodeData?: any) => {
+        onClose(nodeType, nodeData)
     }
 
-    const NodeItem = ({ label, style, nodeType }: { label: string, style: any, nodeType: NodeTypes }) => (
+    const NodeItem = (
+        { label, style, nodeType, icon, nodeData }: { label: string, style: any, icon?: React.ReactNode, nodeType: NodeTypes, nodeData?: any }
+    ) => (
         <TooltipProvider>
             <Tooltip>
                 <TooltipTrigger asChild>
                     <Button
                         variant="outline"
-                        className="w-full mb-4 p-0 h-auto hover:bg-secondary"
-                        onClick={() => handleNodeSelected(nodeType)}
+                        className="w-full h-full p-0 hover:bg-secondary"
+                        onClick={() => handleNodeSelected(nodeType, nodeData)}
                     >
-                        <div className="flex flex-col items-center py-3">
-                            <span className="text-sm mb-2">{label}</span>
-                            <div style={{ ...style }} className="border-2 border-primary" />
+                        <div
+                            style={{ ...style }}
+                            className="flex my-3 justify-center items-center border-2 border-primary cursor-move"
+                        >
+                            { icon }
                         </div>
                     </Button>
                 </TooltipTrigger>
@@ -82,22 +99,46 @@ export default function OnCanvasNodesToolbar(props: OnCanvasNodesToolbarProps) {
                 <DialogHeader className="p-4 pb-2">
                     <DialogTitle>Select Node</DialogTitle>
                 </DialogHeader>
-                <div className="p-4 bg-background flex flex-col items-stretch justify-center">
+                <div className="p-4 bg-background flex flex-col space-y-4 items-stretch justify-center">
                     <NodeItem
                         label="End"
                         style={endNodeShapeStyle}
+                        icon={<Flag className="w-4 h-4 stroke-primary"/>}
                         nodeType={NodeTypes.END_NODE}
-                    />
-                    <NodeItem
-                        label="Activity"
-                        style={activityShapeStyle}
-                        nodeType={NodeTypes.ACTIVITY_NODE}
                     />
                     <NodeItem
                         label="Gateway"
                         style={GatewayShapeStyle}
+                        icon={<Plus className="w-4 h-4 stroke-primary" />}
                         nodeType={NodeTypes.GATEWAY_NODE}
                     />
+                    <div>
+                        <h3 className="text-sm font-semibold mb-2">Activities</h3>
+                        <Input
+                            type="text"
+                            placeholder="Search activities..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className="mb-4"
+                        />
+                        <div className="max-h-60 overflow-y-auto">
+                            <div className="flex flex-row gap-x-4 gap-y-4 flex-wrap">
+                                {[...filteredActivities].map((nodeDefinitionPreview) => (
+                                    <NodeItem
+                                        key={`Activity-Node-${nodeDefinitionPreview.id}`}
+                                        label={nodeDefinitionPreview.name}
+                                        icon={<DynamicIcon name={nodeDefinitionPreview.icon}
+                                                           className="stroke-primary"/>}
+                                        style={activityShapeStyle}
+                                        nodeType={NodeTypes.ACTIVITY_NODE}
+                                        nodeData={{
+                                            nodeDefinitionId: nodeDefinitionPreview.id,
+                                        }}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </DialogContent>
         </Dialog>
