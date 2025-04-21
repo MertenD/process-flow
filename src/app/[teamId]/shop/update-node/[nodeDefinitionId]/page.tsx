aa577cd3c2a36794cb4f6b580676f2c6b412b4dd
MertenD <merten.dieckmann@web.de>
2025-04-22 01:39:58 +0200

    Remove the first 3 elements from the structure because they are boilerplate and will be added later again (title, description, separator)

--- src/app/[teamId]/shop/update-node/[nodeDefinitionId]/page.tsx
+++ src/app/[teamId]/shop/update-node/[nodeDefinitionId]/page.tsx
@@ -3,6 +3,7 @@ import {createClient} from "@/utils/supabase/server";
 import {redirect} from "next/navigation";
 import CreateNodePage from "@/components/shop/create-node/CreateNodePage";
 import getNodeDefinition from "@/actions/shop/get-node-definition";
+import {NodeDefinition} from "@/model/NodeDefinition";
 
 export default async function CreateNode({ params }: { params: { teamId: number, nodeDefinitionId: number } }) {
 
@@ -14,11 +15,19 @@ export default async function CreateNode({ params }: { params: { teamId: number,
     }
 
     const nodeDefinition = await getNodeDefinition(params.nodeDefinitionId)
+    // Remove the first 3 elements from the structure because they are boilerplate and will be added later again (title, description, separator)
+    const filteredNodeDefinition = {
+        ...nodeDefinition,
+        optionsDefinition: {
+            ...nodeDefinition.optionsDefinition,
+            structure: nodeDefinition.optionsDefinition.structure.slice(3)
+        }
+    } as NodeDefinition
 
     return <CreateNodePage
         teamId={params.teamId}
         userId={userData.user.id}
-        initialNodeDefinition={nodeDefinition}
+        initialNodeDefinition={filteredNodeDefinition}
         nodeDefinitionId={params.nodeDefinitionId}
     />
 }
